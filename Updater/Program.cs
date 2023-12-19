1d026ff071b709b201edb2af7ac9ad7146f5931d
MelekTaus Moribundus <melektaus@outlook.at>
2023-12-19 15:27:07 +0100

    Add Updater, Fix DNS Cache

---
+++ Updater/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using System.Security.Principal;
+
+var pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+
+if (!pricipal.IsInRole(WindowsBuiltInRole.Administrator))
+{
+    Process.Start(new ProcessStartInfo
+    {
+        FileName = "Updater.exe",
+        UseShellExecute = true,
+        Verb = "runas",
+    });
+    return;
+}
+
+await Process.Start("cmd", "/c net stop Brandmauer").WaitForExitAsync();
+
+try
+{
+    var processPath = Environment.ProcessPath;
+    Environment.CurrentDirectory = Path.GetDirectoryName(processPath);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    goto Exit;
+}
+
+using (var httpClient = new HttpClient())
+{
+    try
+    {
+        using var stream = await httpClient.GetStreamAsync(
+            "https://steinalt.online/download/brandmauer/windows.zip"
+        );
+        using var zip = new ZipArchive(stream);
+        zip.ExtractToDirectory(".", true);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
+
+Exit:
+await Process.Start("cmd", "/c net start Brandmauer").WaitForExitAsync();
