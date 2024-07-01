5ef1979f3c8b3d5ee1dc9fdba84b47a8e554d629
MelekTaus Moribundus <melektaus@outlook.at>
2024-07-01 20:13:10 +0200

    ** New Menu **

--- wwwroot/static/script/100-main.js
+++ wwwroot/static/script/100-main.js
@@ -1,3 +1,21 @@
+{
+    (function()
+    {
+        
+        if (!LINUX)
+        {
+            qAll(`[data-target-page="build"]`).forEach($ => $.remove())
+        }
+        
+        if (WINDOWS)
+        {
+            qAll(`[data-target-page="services"]`).forEach($ => $.remove())
+            qAll(`[data-target-page="rules"]`).forEach($ => $.remove())
+            qAll(`[data-target-page="natroutes"]`).forEach($ => $.remove())
+        }
+        
+    })()
+}
 
 Interactive.init()
 
@@ -27,29 +45,122 @@ InteractiveAction.gotoPage(
             $ripple.remove()
         })
         
-        qAll(`li[data-action="gotoPage"][data-target="build"].display-none`).forEach(
-            $ =>
-            {
-                if (!LINUX)
-                {
-                    $.remove()
-                    return
-                }
-                
-                $.setClass(`display-none`, false)
-        })
+    })()
+}
+
+{
+    (function()
+    {
+        function getCssFloat(name)
+        {
+            const style = getComputedStyle(document.documentElement)
+            return parseFloat(style.getPropertyValue(name))
+        }
         
-        qAll(`li[data-action="gotoPage"][data-target].display-none`).forEach(
-            $ =>
+        class Menu
+        {
+            #menu
+            #bar
+            #indicator
+            #items = []
+            
+            constructor()
             {
-                if (WINDOWS)
+                this.#menu = q(`.menu`)
+                this.#bar = this.#menu.q(`.menu-bar`)
+                
+                on("mouseup", () =>
                 {
-                    $.remove()
+                    this.#bar.qAll(".icon").forEach($ => $.setClass("active", false))
+                })
+                
+                this.#indicator = this.#menu.q(`.menu-indicator`)
+                
+                if (!this.#bar.children.length)
                     return
+                
+                for (const item of this.#bar.children)
+                {
+                    this.#items.push({
+                        label: item.dataset.label,
+                        icon: item.dataset.icon,
+                        targetPage: item.dataset.targetPage,
+                        active: item.getAttribute(`active`) !== null
+                    })
                 }
                 
-                $.setClass(`display-none`, false)
+                this.#bar.innerHTML = ``
+                
+                for (const item of this.#items)
+                {
+                    const $ = this.#bar.create(`div`).setClass(`menu-bar-item`)
+                    const html = `<i class="fa fa-${item.icon}"></i>`
+                    $.dataset.action = `gotoPage`
+                    $.dataset.target = item.targetPage
+                    $.create(`div`).setClass(`icon`).setHtml(html)
+                    $.create(`div`).setClass(`label`).setHtml(item.label)
+                    $.create(`div`).setClass(`hover-label`).setHtml(item.label)
+                }
+                
+                const items = [...this.#bar.children]
+                for (const i in items)
+                    this.#setupBarItem(items, items[i], i)
+                
+                setTimeout(() =>
+                {
+                    this.#indicator.style.transition = `none`
+                    
+                    for (const i in items)
+                        if (items[i].hasClass(`active`))
+                            this.#setIndicatorOffset(i)
+                    
+                    setTimeout(() =>
+                    {
+                        this.#indicator.setClass(`display-none`, false)
+                        this.#indicator.style.transition = null
+                    })
+                })
             }
-        )
+            
+            #setupBarItem(items, item, i)
+            {
+                item.on(`mousedown`, () => item.q(".icon").setClass(`active`))
+                
+                const activate = () =>
+                {
+                    if (item.hasClass(`active`))
+                        return
+                    
+                    items.forEach((item, j) => item.setClass(`active`, i == j))
+                    
+                    this.#setIndicatorOffset(i)
+                }
+                
+                item.on(`click`, activate)
+                
+                if (this.#items[i].active)
+                    activate()
+            }
+            
+            #setIndicatorOffset(i)
+            {
+                const l = this.#bar.children.length
+                
+                let s = getCssFloat("--menu-item-size")
+                s += getCssFloat("--menu-item-spacing")
+                
+                let offset = Math.ceil(l / 2) * s - s / 2
+                
+                if (l % 2 == 1)
+                    offset -= s / 2
+                
+                offset -= i * s
+                
+                this.#indicator.style.transform = `translateX(${-offset}rem)`
+            }
+        }
+        
+        new Menu
+        
     })()
 }
