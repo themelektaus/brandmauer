a24e50466676901c3ac34ee5b218a8b050d53afb
MelekTaus Moribundus <melektaus@outlook.at>
2024-07-23 13:33:27 +0200

    Auto detect script content type

--- wwwroot/static/script/060-page-reverseproxyroutes.js
+++ wwwroot/static/script/060-page-reverseproxyroutes.js
@@ -2,17 +2,13 @@ class ReverseproxyroutesPage extends ListPage
 {
     static _ = Page.register(this)
     
-    async refresh()
+    async createNode(item)
     {
-        await super.refresh()
+        await super.createNode(item)
         
-        for (const item of this.items)
-        {
-            const id = item.model.identifier.id
-            const $ = item.$.q(`.edit-script`)
-            $.style.display = null
-            $.innerHTML = item.model.script ? `Edit Script` : `Create Script`
-            $.href = `api/reverseproxyroutes/${id}?format=html`
-        }
+        const id = item.model.identifier.id
+        const $ = item.$.q(`.edit-script`)
+        $.innerHTML = item.model.script ? `Edit Script` : `Create Script`
+        $.href = `api/reverseproxyroutes/${id}?format=html`
     }
 }
